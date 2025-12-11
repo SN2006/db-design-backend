396f9f19c2c2399bc8e98935dd70fc8c6e7b271e
Viacheslav Niedieliev <viacheslav_niedieliev@epam.com>
2025-12-11 09:45:44 +0200

    Add opportunity to get user`s profile

--- src/main/java/com/example/app/dbdesignbackend/controller/AuthController.java
+++ src/main/java/com/example/app/dbdesignbackend/controller/AuthController.java
@@ -4,11 +4,15 @@ import com.example.app.dbdesignbackend.dto.CredentialDTO;
 import com.example.app.dbdesignbackend.dto.LoginResponseDTO;
 import com.example.app.dbdesignbackend.dto.RegistrationDTO;
 import com.example.app.dbdesignbackend.security.Role;
+import com.example.app.dbdesignbackend.security.User;
 import com.example.app.dbdesignbackend.service.LoginService;
 import com.example.app.dbdesignbackend.service.RegisterService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,13 @@ public class AuthController {
 
     private LoginService loginService;
     private RegisterService registerService;
+
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<User> getProfile(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(user);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid CredentialDTO credentialDTO) {
